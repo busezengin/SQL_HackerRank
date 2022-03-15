7b15ba5ffe96a539577ed7a85d26196edaaea589
Buse Zengin <buse_zengin@hotmail.com>
2022-03-15 19:01:11 +0300

    Creating Solution.sql
    Solution of The PADS problem

--- Solution.sql
+++ Solution.sql
@@ -2,5 +2,5 @@ SELECT CONCAT(Name, '(',LEFT(Occupation, 1),')') FROM OCCUPATIONS
 ORDER BY Name ASC;
 
 SELECT CONCAT('There are a total of ', COUNT(*), ' ', LOWER(Occupation),'s.') FROM OCCUPATIONS
-GROUP BY Occupation
+GROUP BY Occupation 
 ORDER BY COUNT(*) ASC;
